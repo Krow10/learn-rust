54d50022ab8145f80f26dbf141ec5516f5f4941e
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-10-23 06:36:07 -0400

    Implement sample code for RNG

--- slot_machine/src/main.rs
+++ slot_machine/src/main.rs
@@ -1,3 +1,24 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let rng_iter = rand::thread_rng().sample_iter(rand::distributions::Standard);
+        /*
+            Real-life slot machines stores the result before user starts the spin. That way, you can program the display according
+            to the pre-calculated result with certain features such as showing a particular sound, visual etc. Note that certain
+            regulations (US) do not allow to program such things as "near-miss" (that could incitivise player to play more thinking it
+            "almost" won) based on the pre-calculated result.
+        */
+        let spin_result: Vec<u32> = rng_iter.take(3).collect();
+        println!("Enter any input to start a spin!");
+
+        let mut user_input = String::new();
+
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read user input");
+
+        println!("{:?}", spin_result);
+    }
 }
